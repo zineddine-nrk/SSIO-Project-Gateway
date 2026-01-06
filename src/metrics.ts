70201735496ec9d623bdc4f71b39c4cf84317ee3
zineddine-nrk <nirakzineddine@gmail.com>
2026-01-06 01:45:24 +0100

    erreur 1 dockerfile2

--- src/metrics.ts
+++
@@ -1,6 +0,0 @@
-import * as client from 'prom-client';
-
-const register = new client.Registry();
-client.collectDefaultMetrics({ register }); // metrics automatiques Node.js
-
-export default register;
